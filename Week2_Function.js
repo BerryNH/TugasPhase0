8d895214cb558a4cac2c61ae33f35ac74d97702f
BerryNH <44157194+BerryNH@users.noreply.github.com>
2018-10-26 07:13:04 +0700

    up up up

---
+++ Week2_Function.js
@@ -0,0 +1,38 @@
+
+console.log('soal 1');
+
+function shoutout() {
+  return 'Halo Function!';
+}
+
+console.log(shoutout());
+
+console.log('\n');
+
+console.log('soal 2');
+
+function calculateMultiply(num1,num2) {
+  return num1*num2;
+}
+
+var num1 = 5;
+var num2 = 6;
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+console.log('\n');
+
+console.log('soal 3');
+
+function processSentence(name,age,address,hobby) {
+  return 'Nama saya '+name+' umur saya '+age+' tahun, alamat saya di '+address+' dan saya punya hobby yaitu '+hobby+' !';
+
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
